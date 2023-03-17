a5cf4cc3632de6358f834e94200efc76bc529908
Felix Perktold <felix@perktold.xyz>
2023-03-17 11:52:55 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<PlayerCanvas />
+</template>
+
+<script>
+import PlayerCanvas from './components/PlayerCanvas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PlayerCanvas
+  }
+}
+</script>
---
+++ src/components/PlayerCanvas.vue
@@ -0,0 +1,14 @@
+<template>
+  <vue-drawing-canvas ref="VueCanvasDrawing" />
+</template>
+
+<script>
+  import VueDrawingCanvas from "vue-drawing-canvas";
+
+  export default {
+    name: "PlayerCanvas",
+    components: {
+      VueDrawingCanvas,
+    },
+  };
+</script>
